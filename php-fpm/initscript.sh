321ccce11436b93b82cb540ce3f44d0d50ef3b37
aby <anti1346@mt.co.kr>
2022-06-15 11:31:34 +0900

    no message

--- php-fpm/initscript.sh
+++ php-fpm/initscript.sh
@@ -2,6 +2,37 @@
   
 set -m
 
+### WebService Account
+RUN groupadd -g 33 www-data \
+    && useradd -u 33 -ms /usr/sbin/nologin -g www-data www-data
+
+###php.ini, php-fpm.conf, www.conf setting
+PHPINI="/usr/local/etc/php/php.ini"
+PHPFPMCONF="/usr/local/etc/php-fpm.conf"
+WWWCONF="/usr/local/etc/php-fpm.d/www.conf"
+
+cp /usr/local/etc/php/php.ini-production ${PHPINI}
+sed -i "s/display_errors = Off/display_errors = On/" ${PHPINI}
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 10M/" ${PHPINI}
+sed -i "s/post_max_size = .*/post_max_size = 12M/" ${PHPINI}
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" ${PHPINI}
+sed -i "s/variables_order = .*/variables_order = 'EGPCS'/" ${PHPINI}
+
+sed -i "s/;daemonize\s*=\s*yes/daemonize = no/g" ${PHPFPMCONF}
+sed -i "s/;error_log =.*/error_log = \/proc\/self\/fd\/2/" ${PHPFPMCONF}
+
+sed -i "s/;listen.owner = www-data/listen.owner = www-data/g" ${WWWCONF}
+sed -i "s/;listen.group = www-data/listen.group = www-data/g" ${WWWCONF}
+sed -i "s/user = www-data/user = www-data/g" ${WWWCONF}
+sed -i "s/group = www-data/group = www-data/g" ${WWWCONF}
+sed -i "s/listen = .*/listen = 9000/" ${WWWCONF}
+sed -i "s/pm.max_children = .*/pm.max_children = 200/" ${WWWCONF}
+sed -i "s/pm.start_servers = .*/pm.start_servers = 56/" ${WWWCONF}
+sed -i "s/pm.min_spare_servers = .*/pm.min_spare_servers = 32/" ${WWWCONF}
+sed -i "s/pm.max_spare_servers = .*/pm.max_spare_servers = 96/" ${WWWCONF}
+sed -i "s/^;clear_env = no$/clear_env = no/" ${WWWCONF}
+
+
 ###laravel initialize(default laravel page)
 LDIR="/var/www/html/laravel"
 if [[ $(find /var/www/html -name .env -type f | wc -l) -eq 0 ]]; then
