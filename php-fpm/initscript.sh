52a283cf71ec102d970a2b51def83b9d101264bc
aby <anti1346@mt.co.kr>
2022-06-15 11:13:02 +0900

    no message

--- php-fpm/initscript.sh
+++ php-fpm/initscript.sh
@@ -2,11 +2,19 @@
   
 set -m
 
-###laravel initialize
-cd /var/www/html
-composer global require laravel/installer
-composer create-project --prefer-dist laravel/laravel laravel
-composer install
-chown -R www-data:www-data /var/www/html/laravel
-chmod -R 775 /var/www/html/laravel/bootstrap/cache
-chmod -R 755 /var/www/html/laravel/storage
+###laravel initialize(default laravel page)
+LDIR="/var/www/html/laravel"
+if [[ $(find /var/www/html -name .env -type f | wc -l) -eq 0 ]]; then
+    echo "laravel is not found(empty)"
+    cd /var/www/html
+    composer global require laravel/installer
+    composer create-project laravel/laravel laravel --prefer-dist
+    # cd $LDIR
+    # composer install
+    chown -R www-data:www-data $LDIR
+    chmod -R 775 $LDIR/bootstrap/cache
+    chmod -R 755 $LDIR/storage
+    exit 127
+else
+  echo "laravel found(not empty)"
+fi
